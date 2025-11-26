ba09dbae2a360591e2fee71c76828582ce01a33b
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-11-26 11:44:40 +0530

    Play Sounds on a Website

--- 04 DOM Practice/01 Drum Kit Project/index.js
+++ 04 DOM Practice/01 Drum Kit Project/index.js
@@ -1,15 +1,20 @@
 // alert("Welcom")
 
-var numberOfDrumButtons = document.querySelectorAll(".drum").length
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I Got Clicked.");
-        }
-    );
+        
+        // consoloe.log(this.innerHTML);
+
+        this.style.color = "white";
+
+    });
 
 }
 
 
 
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play()
